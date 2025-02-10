b750953f9923f02fa59e1a5489a2bb9fa70e0fa7
Unleashed01 <b421014@iiit-bh.ac.in>
2025-02-10 19:14:08 +0530

    checking

--- src/App.jsx
+++ src/App.jsx
@@ -239,7 +239,7 @@ function App() {
                     }
                 />
                 <Route
-                    path="login"
+                    path="/login"
                     element={
                         <>
                             <Header link={"/img/header/batch.jpg"}/>
