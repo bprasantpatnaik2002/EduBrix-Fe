4d98dd21961833ba7a9df6182e67441be15ff307
Unleashed01 <b421014@iiit-bh.ac.in>
2025-03-03 11:55:33 +0530

    Replace Google logo image with Font Awesome icon in Testimonials component

--- src/Components/Home/Testimonials.jsx
+++ src/Components/Home/Testimonials.jsx
@@ -24,7 +24,7 @@ function Testimonials() {
                         <div className="testimonial-meta"> - {d.name} </div>
                           <a href={d.link} target="_blank">
                           <div class="google-g">
-                              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png" alt="Google G" /> 
+                            <i class="fa fa-google" aria-hidden="true" /> 
                           </div>
                           </a>
                         </div>
