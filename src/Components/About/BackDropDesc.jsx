1a90bc5fac423f7beab27584836c9cea5236be86
Unleashed01 <b421014@iiit-bh.ac.in>
2025-03-05 16:52:10 +0530

    Remove redundant text from designation in BackDropDesc component

--- src/Components/About/BackDropDesc.jsx
+++ src/Components/About/BackDropDesc.jsx
@@ -6,7 +6,7 @@ function BackDropDesc({name,image,job,desc,account}) {
         <div className='Member'>
             <div className='name'>{name}</div>
             <img src={image} alt={`${name} image`} />
-            <div className='designation'><span className='firstPart'>Director -</span><span className='secondPart'>- {job}</span></div>
+            <div className='designation'><span className='firstPart'>Designation</span><span className='secondPart'>- {job}</span></div>
             <div className="designation">
             <p>
                 <a href={account} className="linkedin-link">
