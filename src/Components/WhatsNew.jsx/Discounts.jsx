e249180cf0c7d90989bef737b5e1dd89aa9ced42
Unleashed01 <b421014@iiit-bh.ac.in>
2025-03-07 11:10:04 +0530

    Enhance Coupons section styling with gradient background and clean up code

--- src/Components/WhatsNew.jsx/Discounts.jsx
+++ src/Components/WhatsNew.jsx/Discounts.jsx
@@ -68,6 +68,7 @@ function Coupons() {
                                     </button>
                                 </div>
                             </div>
+                            
                         </div>
                     ))}
                 </Slider>
